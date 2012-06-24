caaed05f49eb2822ec8c2168f960f33d4dd6cbba
Chris Monaccio <billsandaccounts@chrismonaccio.com>
2012-06-23 23:30:15 -0400

    Adding The Parties content.

--- the-parties.php
+++ the-parties.php
@@ -5,7 +5,7 @@
 
     <div id="page" class="clear" >
       <div id="content" class="clear" >
-        <h1 class="tk-adobe-garamond-pro" >Welcome</h1>
+        <h1>The Parties</h1>
 
       </div><!-- #content -->
     </div><!-- #page -->
