46eaacef55d1060daeac0b8d225c89925cc79c18
Chris Monaccio <billsandaccounts@chrismonaccio.com>
2012-06-25 11:47:58 -0400

    Adding title graphics.

--- our-story.php
+++ our-story.php
@@ -9,6 +9,7 @@
         <div id="the-couple" class="aligncenter" >
           <img src="img/our-story.png" alt="Chris & Christine: Our Story" >
         </div>
+        <h3 class="aligncenter" >How We Met</h3>
         <p>We’ve known each other for about 8 years. In that time we’ve been together and we’ve been apart. Early on I left for school and we both graduated and started our careers.</p>
         <p>Surprisingly, we reconnected at just the right time. Having fallen for each other a second time, I moved back to Virginia and after dating for a few years, we moved into our first apartment together. Three years ago, on New Year’s Eve, I proposed to Christine after watching our favorite Pixar movie, “Up”.</p>
         <p>In all this time we’ve grown up quite a bit. We’ve lost jobs and very dear family members. Though they can’t be here, we’re excited to finally be married and start living the rest of our lives together.</p>
