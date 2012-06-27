a60c3f6a3c5e8e22d106642e6a88fec244c28bc8
Chris Monaccio <chris@chrismonaccio.com>
2012-06-26 22:38:11 -0400

    Adding all of Chrissy's copywriting.

--- the-parties.php
+++ the-parties.php
@@ -18,16 +18,19 @@
               <img src="img/profile-pic-drew.png" alt="Drew Cline" >
               <h3>Drew Cline</h3>
               <p><em>Best Man</em></p>
+              <p>Friend of the Groom. Drew and I met at North Stafford High School after our family moved to VA. Drew also attended Full Sail University with me shortly after I began classes.</p>
             </div><!-- #drew -->
 
             <div id="steve" class="party-member aligncenter" >
               <img src="img/profile-pic-steve.png" alt="Steve Celestin" >
               <h3>Steve Celestin</h3>
+              <p>Steve and I met while our family was stationed at Patrick Air Force Base in Cocoa Beach, FL. We attended Cocoa Beach Jr/Sr High School together and were later roommates in Winter Park while attending Full Sail University.
             </div><!-- #steve -->
 
             <div id="jon" class="party-member aligncenter" >
               <img src="img/profile-pic-jon.png" alt="Jon Mills" >
               <h3>Jon Mills</h3>
+              <p>Jon is Jen Beidel's (Bridesmaid) fiance. All of us met while we were living at our first apartment together in Woodbridge, VA. Jon and Jen were our neighbors directly across from us at the time.</p>
             </div><!-- #jon -->
 
           </div><!-- #col-1 -->
@@ -42,28 +45,32 @@
               <img src="img/profile-pic-kaycee.png" alt="Kaycee Nakamoto" >
               <h3>Kaycee Nakamoto</h3>
               <p><em>Maid of Honor</em></p>
+              <p>Kaycee and I met in middle school and became close friends post-college.</p>
             </div><!-- #kaycee -->
 
             <div id="jen" class="party-member aligncenter" >
               <img src="img/profile-pic-jen.png" alt="Jen Beidel" >
               <h3>Jen Beidel</h3>
+              <p>Jen is Jon Mills' (Groomsman) fiance. All of us met while we were living at our first apartment together in Woodbridge, VA. Jon and Jen were our neighbors directly across from us at the time.</p>
             </div><!-- #jen -->
 
             <div id="caitie" class="party-member aligncenter" >
               <img src="img/profile-pic-caitie.png" alt="Caitie Monaccio" >
               <h3>Caitie Monaccio</h3>
+              <p>Caitie is Chris' sister and soon to be sister-in-law to the bride.</p>
             </div><!-- #caitie -->
 
           </div><!-- #col-2 -->
 
           <div class="ribbon" >
-              <img src="img/officiant.png" alt="Officiant" />
-            </div><!-- .ribbon -->
+            <img src="img/officiant.png" alt="Officiant" />
+          </div><!-- .ribbon -->
           <div id="dominica" class="party-member aligncenter" >
-              <img src="img/profile-pic-dominica.png" alt="Dominica Williams" >
-              <h3>Dominica Williams</h3>
-              <p><em>Officiant</em></p>
-            </div><!-- #dominica -->
+            <img src="img/profile-pic-dominica.png" alt="Dominica Williams" >
+            <h3>Dominica Williams</h3>
+            <p><em>Officiant</em></p>
+            <p>Dominica and Christine met while working at FACETS, a non-profit in Fairfax, VA.
+          </div><!-- #dominica -->
 
         </div><!-- #parties -->
 
