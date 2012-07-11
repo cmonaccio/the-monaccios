aad1e86a19127fa6b8188ab7368efda7a930ce97
Chris Monaccio <billsandaccounts@chrismonaccio.com>
2012-07-10 21:35:59 -0400

    Adding Welcome page graphic, final styling.

--- index.php
+++ index.php
@@ -4,12 +4,17 @@
     <?php include 'nav.php'; ?>
 
     <div id="page" class="clear" >
-      <div id="content" class="clear" >
+      <div id="content" class="welcome clear" >
         
+        <figure class="aligncenter">
+          <img src="img/chris-and-chrissy.png" alt="">
+        </figure>
+
         <div id="time-and-date" class="aligncenter" >
+          <div id="top-rule"></div>
           <p>Are getting married<br>in Fairfax City, VA<br>
           <span id="month-day" >August Eleventh</span><br>
-          <span id="numeric-date" >08 • 11 • 2012</span></p>
+          <span id="numeric-date" >08 &bull; 11 &bull; 2012</span></p>
         </div>
 
       </div><!-- #content -->
