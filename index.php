7bff72c5708740902ddf57e8a71d92f6d880137c
Chris Monaccio <billsandaccounts@chrismonaccio.com>
2012-07-10 21:07:43 -0400

    Fixing up welcome page.

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
     <div id="page" class="clear" >
       <div id="content" class="clear" >
         
+        <div id="time-and-date" class="aligncenter" >
+          <p>Are getting married<br>in Fairfax City, VA<br>
+          <span id="month-day" >August Eleventh</span><br>
+          <span id="numeric-date" >08 • 11 • 2012</span></p>
+        </div>
 
       </div><!-- #content -->
     </div><!-- #page -->
