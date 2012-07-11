6ce2fd356eb87d8f419867c8dea4a508ce703ed2
Chris Monaccio <billsandaccounts@chrismonaccio.com>
2012-07-10 21:51:44 -0400

    Temporarily disabling Our Story page.

--- nav.php
+++ nav.php
@@ -2,7 +2,7 @@
 	<nav id="nav-prime" >
 		<ul class="clear" >
 			<li><a href="index.php">Welcome</a></li>
-			<li><a href="our-story.php">Our Story</a></li>
+			<!-- <li><a href="our-story.php">Our Story</a></li> -->
 			<li><a href="the-parties.php">The Parties</a></li>
 			<li><a href="ceremony.php">Ceremony</a></li>
 			<li><a href="travel.php">Travel</a></li>
